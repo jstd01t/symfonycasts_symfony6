eee98ba2b5fdc8f8540634f83cb4c24b6a8a2408
jstd01t <commontasksfor@gmail.com>
2022-05-02 17:23:25 +0300

    add controller

---
+++ src/Controller/VinylController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+
+class VinylController
+{
+    #[Route('/')]
+    public function homepage()
+    {
+        die('helo');
+    }
+
+}
